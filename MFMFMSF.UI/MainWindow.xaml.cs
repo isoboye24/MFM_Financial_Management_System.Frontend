dce4062da1d7398c2d496154a4c3bcaf419a8de9
isoboye24 <isobarguy4real@gmail.com>
2026-07-25 05:47:06 +0200

    Logo section added on the side bar

--- MFMFMSF.UI/MainWindow.xaml.cs
+++ MFMFMSF.UI/MainWindow.xaml.cs
@@ -11,14 +11,32 @@ using System.Windows.Shapes;
 
 namespace MFMFMSF.UI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            TopBarControl.MinimizeRequested += TopBar_MinimizeRequested;
+            TopBarControl.MaximizeRequested += TopBar_MaximizeRequested;
+            TopBarControl.CloseRequested += TopBar_CloseRequested;
+        }
+
+        private void TopBar_MinimizeRequested(object? sender, EventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void TopBar_MaximizeRequested(object? sender, EventArgs e)
+        {
+            WindowState = WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+        }
+
+        private void TopBar_CloseRequested(object? sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
