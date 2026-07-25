285c0c98342c2034d16574ac3ff6a087db4376fa
isoboye24 <isobarguy4real@gmail.com>
2026-07-25 19:41:21 +0200

    Sidebar menu items created

---
+++ MFMFMSF.UI/Controls/SidebarMenuItem.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MFMFMSF.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for SidebarMenuItem.xaml
+    /// </summary>
+    public partial class SidebarMenuItem : UserControl
+    {
+        public SidebarMenuItem()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(
+                nameof(Title),
+                typeof(string),
+                typeof(SidebarMenuItem),
+                new PropertyMetadata(string.Empty, OnTitleChanged));
+
+        private static void OnTitleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((SidebarMenuItem)d).TitleText.Text = e.NewValue?.ToString();
+        }
+
+        public string Icon
+        {
+            get => (string)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(
+                nameof(Icon),
+                typeof(string),
+                typeof(SidebarMenuItem),
+                new PropertyMetadata(string.Empty, OnIconChanged));
+
+        private static void OnIconChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((SidebarMenuItem)d).IconText.Text = e.NewValue?.ToString();
+        }
+
+        public bool IsSelected
+        {
+            get => (bool)GetValue(IsSelectedProperty);
+            set => SetValue(IsSelectedProperty, value);
+        }
+
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register(
+                nameof(IsSelected),
+                typeof(bool),
+                typeof(SidebarMenuItem),
+                new PropertyMetadata(false, OnSelectedChanged));
+
+        private static void OnSelectedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = (SidebarMenuItem)d;
+
+            control.MenuBorder.Background =
+                (bool)e.NewValue
+                    ? new System.Windows.Media.SolidColorBrush(
+                        (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#5B4CF3"))
+                    : System.Windows.Media.Brushes.Transparent;
+        }
+    }
+}
