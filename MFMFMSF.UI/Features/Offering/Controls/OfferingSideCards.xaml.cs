14741447dd668645df23f272f332005c87cf7101
isoboye24 <isobarguy4real@gmail.com>
2026-08-24 16:31:07 +0200

    Offering table and offering side cards components created.

---
+++ MFMFMSF.UI/Features/Offering/Controls/OfferingSideCards.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Controls;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
+namespace MFMFMSF.UI.Features.Offering.Controls
+{
+    public partial class OfferingSideCards : UserControl
+    {
+        public ISeries[] OfferingSeries { get; set; }
+
+        public OfferingSideCards()
+        {
+            InitializeComponent();
+
+            OfferingSeries = [
+                                new PieSeries<double>
+                                {
+                                    Values = [18620],
+                                    InnerRadius = 28
+                                },
+
+                                new PieSeries<double>
+                                {
+                                    Values = [28450],
+                                    InnerRadius = 28
+                                },
+
+                                new PieSeries<double>
+                                {
+                                    Values = [6870],
+                                    InnerRadius = 28
+                                },
+
+                                new PieSeries<double>
+                                {
+                                    Values = [2960],
+                                    InnerRadius = 28
+                                }
+                            ];
+
+            DataContext = this;
+        }
+    }
+}
