96229cf5e14793d2a63879e2a6d6c426e8fe6c2d
isoboye24 <isobarguy4real@gmail.com>
2026-08-27 00:02:13 +0200

    Tithe side cards component added to the tithe page.

---
+++ MFMFMSF.UI/Features/Tithes/Controls/TitheSideCards.xaml.cs
@@ -0,0 +1,60 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MFMFMSF.UI.Features.Tithes.Controls
+{
+    /// <summary>
+    /// Interaction logic for TitheSideCards.xaml
+    /// </summary>
+    public partial class TitheSideCards : UserControl
+    {
+        public ISeries[] TitheSeries { get; set; }
+
+        public TitheSideCards()
+        {
+            InitializeComponent();
+
+            TitheSeries = [
+                                new PieSeries<double>
+                                {
+                                    Values = [18620],
+                                    InnerRadius = 28
+                                },
+
+                                new PieSeries<double>
+                                {
+                                    Values = [28450],
+                                    InnerRadius = 28
+                                },
+
+                                new PieSeries<double>
+                                {
+                                    Values = [6870],
+                                    InnerRadius = 28
+                                },
+
+                                new PieSeries<double>
+                                {
+                                    Values = [2960],
+                                    InnerRadius = 28
+                                }
+                            ];
+
+            DataContext = this;
+        }
+    }
+}
