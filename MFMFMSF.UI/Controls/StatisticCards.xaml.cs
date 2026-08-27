e0f6e4d8c695d197fd7bd197bf465e0e1604a314
isoboye24 <isobarguy4real@gmail.com>
2026-08-28 00:33:51 +0200

    I moved the statistic cards component to global and added it to expenditures page.

---
+++ MFMFMSF.UI/Controls/StatisticCards.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MFMFMSF.UI.Controls
+{
+    /// <summary>
+    /// Interaction logic for StatisticCard.xaml
+    /// </summary>
+    public partial class StatisticCards : UserControl
+    {
+        public StatisticCards()
+        {
+            InitializeComponent();
+        }
+    }
+}
