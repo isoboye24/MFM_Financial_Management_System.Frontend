cefbb901f7b812a3d75632d0089b4b331590f9e2
isoboye24 <isobarguy4real@gmail.com>
2026-08-27 14:34:10 +0200

    Other income page created.

--- MFMFMSF.UI/ViewModels/SidebarViewModels.cs
+++ MFMFMSF.UI/ViewModels/SidebarViewModels.cs
@@ -2,6 +2,7 @@
 using MFMFMSF.UI.Features.Dashboard.Views;
 using MFMFMSF.UI.Features.Members.Views;
 using MFMFMSF.UI.Features.Offering.Views;
+using MFMFMSF.UI.Features.OtherIncome.Views;
 using MFMFMSF.UI.Features.Seeds.Views;
 using MFMFMSF.UI.Features.Tithes.Views;
 using MFMFMSF.UI.Navigation;
@@ -17,6 +18,7 @@ namespace MFMFMSF.UI.Features.Dashboard.ViewModels
         public ICommand NavigateOfferingsCommand { get; }
         public ICommand NavigateTithesCommand { get; }
         public ICommand NavigateSeedsCommand { get; }
+        public ICommand NavigateOtherIncomeCommand { get; }
         public ICommand NavigateMembersCommand { get; }
 
 
@@ -39,6 +41,10 @@ namespace MFMFMSF.UI.Features.Dashboard.ViewModels
             NavigateSeedsCommand =
                 new RelayCommand(_ =>
                     _navigation.Navigate(new SeedsView()));
+
+            NavigateOtherIncomeCommand =
+                new RelayCommand(_ =>
+                    _navigation.Navigate(new OtherIncomeView()));
 
             NavigateMembersCommand =
                 new RelayCommand(_ =>
