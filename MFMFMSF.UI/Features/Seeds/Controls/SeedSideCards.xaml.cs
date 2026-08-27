53a47266319056b47878bd4fcbb6c6ea4fdc5247
isoboye24 <isobarguy4real@gmail.com>
2026-08-27 13:32:25 +0200

    Seed side cards added to the seed page.

---
+++ MFMFMSF.UI/Features/Seeds/Controls/SeedSideCards.xaml.cs
@@ -0,0 +1,47 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using System.Windows.Controls;
+
+namespace MFMFMSF.UI.Features.Seeds.Controls
+{
+    /// <summary>
+    /// Interaction logic for SeedSideCards.xaml
+    /// </summary>
+    public partial class SeedSideCards : UserControl
+    {
+        public ISeries[] SeedSeries { get; set; }
+
+        public SeedSideCards()
+        {
+            InitializeComponent();
+
+            SeedSeries = [
+                                new PieSeries<double>
+                                {
+                                    Values = [18620],
+                                    InnerRadius = 28
+                                },
+
+                                new PieSeries<double>
+                                {
+                                    Values = [28450],
+                                    InnerRadius = 28
+                                },
+
+                                new PieSeries<double>
+                                {
+                                    Values = [6870],
+                                    InnerRadius = 28
+                                },
+
+                                new PieSeries<double>
+                                {
+                                    Values = [2960],
+                                    InnerRadius = 28
+                                }
+                            ];
+
+            DataContext = this;
+        }
+    }
+}
