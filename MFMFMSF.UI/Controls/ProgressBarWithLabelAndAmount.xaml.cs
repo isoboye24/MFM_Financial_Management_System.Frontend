b990ece7c61d667681297daf0f3cf54b2f366f85
isoboye24 <isobarguy4real@gmail.com>
2026-08-27 23:46:36 +0200

    Other income progress bar component created.

---
+++ MFMFMSF.UI/Controls/ProgressBarWithLabelAndAmount.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MFMFMSF.UI.Controls
+{
+    public partial class ProgressBarWithLabelAndAmount : UserControl
+    {
+        public ProgressBarWithLabelAndAmount()
+        {
+            InitializeComponent();
+        }
+
+
+        // =====================================================
+        // LABEL
+        // =====================================================
+
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register(
+                nameof(Label),
+                typeof(string),
+                typeof(ProgressBarWithLabelAndAmount),
+                new PropertyMetadata(string.Empty));
+
+
+        // =====================================================
+        // AMOUNT
+        // =====================================================
+
+        public string Amount
+        {
+            get => (string)GetValue(AmountProperty);
+            set => SetValue(AmountProperty, value);
+        }
+
+        public static readonly DependencyProperty AmountProperty =
+            DependencyProperty.Register(
+                nameof(Amount),
+                typeof(string),
+                typeof(ProgressBarWithLabelAndAmount),
+                new PropertyMetadata(string.Empty));
+
+
+        // =====================================================
+        // PERCENTAGE
+        // =====================================================
+
+        public double Percentage
+        {
+            get => (double)GetValue(PercentageProperty);
+            set => SetValue(PercentageProperty, value);
+        }
+
+        public static readonly DependencyProperty PercentageProperty =
+            DependencyProperty.Register(
+                nameof(Percentage),
+                typeof(double),
+                typeof(ProgressBarWithLabelAndAmount),
+                new PropertyMetadata(0.0));
+
+
+        // =====================================================
+        // BAR FOREGROUND
+        // =====================================================
+
+        public Brush BarForeground
+        {
+            get => (Brush)GetValue(BarForegroundProperty);
+            set => SetValue(BarForegroundProperty, value);
+        }
+
+        public static readonly DependencyProperty BarForegroundProperty =
+            DependencyProperty.Register(
+                nameof(BarForeground),
+                typeof(Brush),
+                typeof(ProgressBarWithLabelAndAmount),
+                new PropertyMetadata(Brushes.MediumPurple));
+    }
+}
---
+++ MFMFMSF.UI/Converters/ProgressWidthConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MFMFMSF.UI.Converters
+{
+    public class ProgressWidthConverter : IMultiValueConverter
+    {
+        public object Convert(
+            object[] values,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            if (values.Length < 2)
+                return 0.0;
+
+            if (values[0] is double percentage &&
+                values[1] is double actualWidth)
+            {
+                return actualWidth * percentage / 100.0;
+            }
+
+            return 0.0;
+        }
+
+
+        public object[] ConvertBack(
+            object value,
+            Type[] targetTypes,
+            object parameter,
+            CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
