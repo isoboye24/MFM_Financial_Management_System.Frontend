3da21c4dae5c78b8f1303caad0e6903eb886921c
isoboye24 <isobarguy4real@gmail.com>
2026-08-30 23:17:04 +0200

    Project side cards and project cards components created.

---
+++ MFMFMSF.UI/Features/Projects/Controls/ProjectSideCards.xaml.cs
@@ -0,0 +1,60 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MFMFMSF.UI.Features.Projects.Controls
+{
+    /// <summary>
+    /// Interaction logic for ProjectSideCards.xaml
+    /// </summary>
+    public partial class ProjectSideCards : UserControl
+    {
+        public ISeries[] ProjectsSeries { get; set; }
+
+        public ProjectSideCards()
+        {
+            InitializeComponent();
+
+            ProjectsSeries = [
+                                new PieSeries<double>
+                                {
+                                    Values = [18620],
+                                    InnerRadius = 28
+                                },
+
+                                new PieSeries<double>
+                                {
+                                    Values = [28450],
+                                    InnerRadius = 28
+                                },
+
+                                new PieSeries<double>
+                                {
+                                    Values = [6870],
+                                    InnerRadius = 28
+                                },
+
+                                new PieSeries<double>
+                                {
+                                    Values = [2960],
+                                    InnerRadius = 28
+                                }
+                            ];
+
+            DataContext = this;
+        }
+    }
+}
