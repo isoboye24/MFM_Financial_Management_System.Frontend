9605c4a88b2f1f9dd42c27d82ffffab42c2f5ced
isoboye24 <isobarguy4real@gmail.com>
2026-08-31 20:47:30 +0200

    Summary by Funds component created.

---
+++ MFMFMSF.UI/Features/Reports/Controls/FinancialSummaryReports/SummaryByFunds.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MFMFMSF.UI.Features.Reports.Controls.FinancialSummaryReports
+{
+    public partial class SummaryByFunds : UserControl
+    {
+        public SummaryByFunds()
+        {
+            InitializeComponent();
+        }
+    }
+}
