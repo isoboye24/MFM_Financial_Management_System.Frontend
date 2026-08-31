a7bc4d98b741f11b863caa6b8449b99de13464e9
isoboye24 <isobarguy4real@gmail.com>
2026-08-31 20:35:58 +0200

    Recent reports component created.

---
+++ MFMFMSF.UI/Features/Reports/Controls/FinancialSummaryReports/RecentReports.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MFMFMSF.UI.Features.Reports.Controls.FinancialSummaryReports
+{
+    /// <summary>
+    /// Interaction logic for RecentReports.xaml
+    /// </summary>
+    public partial class RecentReports : UserControl
+    {
+        public RecentReports()
+        {
+            InitializeComponent();
+        }
+    }
+}
