5d26e52945ebe06bffcb50fbd7f1bf6d865d170b
isoboye24 <isobarguy4real@gmail.com>
2026-09-06 10:59:37 +0200

    Meeting category list component created.

---
+++ MFMFMSF.UI/Features/Settings/Controls/MeetingCategories/MeetingCategoryItem.cs
@@ -0,0 +1,9 @@
+﻿namespace MFMFMSF.UI.Features.Settings.Controls.MeetingCategories
+{
+    public class MeetingCategoryItem
+    {
+        public int Number { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
