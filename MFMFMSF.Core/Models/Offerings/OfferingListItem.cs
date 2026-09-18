59d8d25a51f6fd0125bcb816dbe42efa87d8d303
isoboye24 <isobarguy4real@gmail.com>
2026-09-18 20:33:19 +0200

    Worked on the offering list page.

---
+++ MFMFMSF.Core/Models/Offerings/OfferingListItem.cs
@@ -0,0 +1,12 @@
+﻿namespace MFMFMSF.Core.Models.Offerings
+{
+    public class OfferingListItem
+    {
+        public Guid Id { get; set; }
+        public string MessageTitle { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Minister { get; set; } = string.Empty;
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
