96d134d93af436d1808e2c305ac325b636ddfe39
isoboye24 <isobarguy4real@gmail.com>
2026-09-21 18:22:34 +0200

    Meeting table refactored and redesigned.

---
+++ MFMFMSF.UI/Converters/RowNumberConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MFMFMSF.UI.Converters
+{
+    public class RowNumberConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            if (value == null)
+                return string.Empty;
+
+            if (int.TryParse(value.ToString(), out int index))
+                return (index + 1).ToString();
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
